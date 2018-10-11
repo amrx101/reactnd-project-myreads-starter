b37ca6251ddc4692c3643f435138c07a8d23ac62
Amit <amit@riptideio.com>
2018-10-11 19:21:05 +0530

    Fixed moving a book to currentlyReadingShelf

--- src/Book.js
+++ src/Book.js
@@ -26,7 +26,7 @@ class BookShelfChanger extends React.Component{
   render(){
     return (<div className="book-shelf-changer">
                               <select  onChange={this.changeShelf}>
-                                <option value="move" disabled>Move to...</option>
+                                <option value="move" enabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
                                 <option value="read">Read</option>
